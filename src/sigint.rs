9e780aa70c903c44f73199a3766d50ccd20d527f
Kevin Goslar <kevin.goslar@gmail.com>
2021-01-09 21:38:49 -0600

    progress

--- src/sigint.rs
+++ src/sigint.rs
@@ -2,7 +2,7 @@
 
 use super::signal::Signal;
 
-// captures Ctrl-C and messages it as a Signal::Finish message via the given sender
+// captures Ctrl-C and messages it as a Signal::Exit message via the given sender
 pub fn handle(sender: std::sync::mpsc::Sender<Signal>) {
   ctrlc::set_handler(move || {
     sender.send(Signal::Exit).unwrap();
