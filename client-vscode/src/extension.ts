c36475a450908a58abbf1df36f52bfebe5520261
Kevin Goslar <kevin.goslar@gmail.com>
2021-02-02 19:15:18 -0600

    vscode: stop and repeat command

--- client-vscode/src/extension.ts
+++ client-vscode/src/extension.ts
@@ -8,24 +8,36 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.commands.registerCommand("tertestrial-vscode.testAll", runSafe(testAll)))
   context.subscriptions.push(vscode.commands.registerCommand("tertestrial-vscode.testFile", runSafe(testFile)))
   context.subscriptions.push(vscode.commands.registerCommand("tertestrial-vscode.testLine", runSafe(testLine)))
+  context.subscriptions.push(vscode.commands.registerCommand("tertestrial-vscode.repeatTest", runSafe(repeatTest)))
+  context.subscriptions.push(vscode.commands.registerCommand("tertestrial-vscode.stopTest", runSafe(stopTest)))
 }
 
 async function testAll() {
   notification.display("testing all files")
-  await pipe.send("{}")
+  await pipe.send(`{ "command": "testAll" }`)
 }
 
 async function testFile() {
   const relPath = workspace.currentFile()
   notification.display(`testing file ${relPath}`)
-  await pipe.send(`{"filename": "${relPath}"}`)
+  await pipe.send(`{ "command": "testFile", "filename": "${relPath}" }`)
 }
 
 async function testLine() {
   const relPath = workspace.currentFile()
   const line = workspace.currentLine()
   notification.display(`testing file ${relPath}:${line}`)
-  await pipe.send(`{"filename": "${relPath}", "line": "${line}"}`)
+  await pipe.send(`{ "command": "testLine", "filename": "${relPath}", "line": "${line}" }`)
+}
+
+async function repeatTest() {
+  notification.display("repeating the last test")
+  await pipe.send(`{ "command": "repeatTest" }`)
+}
+
+async function stopTest() {
+  notification.display("stopping the current test")
+  await pipe.send(`{ "command": "stopTest" }`)
 }
 
 function runSafe(f: () => Promise<void>): () => Promise<void> {
